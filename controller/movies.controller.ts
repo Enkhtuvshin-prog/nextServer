46ccc3e16cb2b327ba64a4518641c5c547c51574
Enkhtvshn <Enkhtuwshin.1201@gmail.com>
2023-04-06 19:24:23 +0800

    kk

--- controller/movies.controller.ts
+++ controller/movies.controller.ts
@@ -3,9 +3,17 @@ import { Request, Response } from "express";
 import Movie from "../Model/Movie";
 
 const getMovies = async (req: Request, res: Response) => {
+  const limit = Number(req.query.limit) || 10;
+  const page = Number(req.query.page) || 1;
+
+  const total = await Movie.countDocuments();
+  const pageCount = Math.ceil(total / limit);
+  const skip = (page - 1) * limit;
   try {
-    const movies = await Movie.find().limit(10);
-    res.status(200).json({ success: true, movies });
+    const movies = await Movie.find().skip(skip).limit(limit);
+    res
+      .status(200)
+      .json({ success: true, movies, pagination: { pageCount, total } });
   } catch (err) {
     console.log("ERR++", err);
   }
